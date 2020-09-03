2178a26aa610804dc7ccb50727a2aad5528b0423
Lazar <lakinis97@gmail.com>
2020-09-03 11:11:10 +0200

    delete folder

--- rest-countries-api/src/Webpages/Home.js
+++ rest-countries-api/src/Webpages/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Container } from "../components/Container/Container";
 import Card from "../components/Card/Card";
 import ViewDetails from "./ViewDetails";
